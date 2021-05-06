1a13f90ca946df3c7451ee1c017b991c742a192b
rajesh g <rajesh.ganessan@letsventure.com>
2021-05-06 07:57:46 +0530

    added dataprovider with excel functions

--- src/main/java/com/framework/dataprovider/dataproviderSampleTest.java
+++ src/main/java/com/framework/dataprovider/dataproviderSampleTest.java
@@ -1,15 +1,29 @@
 package com.framework.dataprovider;
 
+import org.apache.poi.xssf.usermodel.XSSFShape;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class dataproviderSampleTest {
 
-    @Test(dataProvider = "getEmployeeData")
+    @Test(dataProvider = "getEmployeeData",enabled = false)
     public void test1(Employee emp){
         System.out.println(emp.getName());
+        System.out.println(System.getProperty("user.dir"));
         Assert.assertTrue(true);
+    }
+
+    @Test(priority = 1,dataProvider = "ExcelDataDump")
+    public void excelTest(String userName,String email,String age,String height, String weight, String gender){
+        System.out.println(userName);
     }
 
     @DataProvider
@@ -26,6 +40,25 @@ public class dataproviderSampleTest {
                new Employee("dinesh","two"),
                 new Employee("ganessan","three")
         };
+    }
+
+    @DataProvider(name = "ExcelDataDump")
+    public Object[] getExcelData() throws IOException {
+        FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/test/java/com/framework/tests/testData.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        XSSFSheet sheet = workbook.getSheet("Login");
+
+        int rowNum  = sheet.getLastRowNum();
+        int colNum  = sheet.getRow(0).getLastCellNum();
+
+        Object[][] data = new Object[rowNum][colNum];
+        for(int i=1;i<=rowNum;i++){
+            for(int j=0;j<colNum;j++){
+                 data[i-1][j] = sheet.getRow(i).getCell(j).getStringCellValue();
+            }
+        }
+
+        return data;
     }
 
 }
